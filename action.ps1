3a6cbd59a93ed3e326e8689c2b0441a79610239c
unknown <nairi.baveyan@rapidsilicon.com>
2023-01-19 13:37:09 +0400

    Added -Path option

--- action.ps1
+++ action.ps1
@@ -7,14 +7,14 @@ $Cygwin = $env:PATH + "bash.exe"
 $arg = "-c"
 
 $YosysDir = Get-Location
-Set-Location ../Raptor_Tools/verific_rs/vhdl_packages
+Set-Location -Path ../Raptor_Tools/verific_rs/vhdl_packages
 
 & $Cygwin $arg "ln -sf vhdl_1987/bin vdbs_1987"
 & $Cygwin $arg "ln -sf vhdl_1993/bin vdbs_1993"
 & $Cygwin $arg "ln -sf vhdl_2008/bin vdbs_2008"
 & $Cygwin $arg "ln -sf vhdl_2019/bin vdbs_2019"
 
-Set-Location $YosysDir
+Set-Location -Path $YosysDir
 & $Cygwin $arg "make genfiles"
 $env:PATH = $OldPath
 
