e8392e6bf429897480b822fbdea4fe02c78fe7fe
komalinayat <komal.inayat@rapidsilicon.com>
2023-07-26 12:21:38 +0500

    Removed previously added condition for Async RAM

--- passes/memory/memory_libmap.cc
+++ passes/memory/memory_libmap.cc
@@ -445,11 +445,6 @@ void MemMapping::determine_style() {
 						mem.set_string_attribute(attr,"logic");
 					}
 			}
-			// if (mem_async_read){
-			// 	log_warning("Asyncronous read in BRAM is not supported, memory will be mapped to soft logic.\n");
-			// 	mem.set_string_attribute(attr,"logic");
-			// }
-			// End: Awais: Fix for EDA-1436: (Map memory with async read to soft logic when inline attribute is block)
 			Const val = mem.attributes.at(attr);
 			if (val == 1) {
 				kind = RamKind::NotLogic;
